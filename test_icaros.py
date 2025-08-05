21e9472583c1655cafa9e4458a14761f589df6b0
mcidlaso <mariacidlaso1@gmail.com>
2025-08-05 18:28:06 +0200

    test

--- test_icaros.py
+++ test_icaros.py
@@ -8,3 +8,18 @@ def diffusion_band(kdst         :   pd.DataFrame,
 
 
     pass
+
+
+
+def test_diffusion_band():
+    Zrms = np.arange(0,10, 0.001)
+    DT = np.arange(0, 2000, 0.001)
+    d = {'Zrms': Zrms, 'DT': DT}
+    df_test = pd.DataFrame(data = d)
+    lower_limit = lambda x: 0.05*x - 3
+    upper_limit = lambda x: 0.05*x + 3
+    fun_test = diffusion_band(df_test, lower_limit, upper_limit)
+
+    assert fun_test.shape == df_test.shape
+
+    assert np.all(fun_test.values == df_test.values)
