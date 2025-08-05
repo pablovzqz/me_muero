c7a592179e968fc269444d08dccf67bdd4efc6a2
mcidlaso <mariacidlaso1@gmail.com>
2025-08-05 19:13:21 +0200

    funcion definida

--- funciones_icaros.py
+++ funciones_icaros.py
@@ -1,11 +1,20 @@
 import pandas as pd
 import numpy as np
+from invisible_cities.core.fit_functions import fit, expo
 
 def f(df: pd.DataFrame,
       bins: np.ndarray,
       nsigma: float,
       ) -> pd.DataFrame:
-    pass
+    meansy , bin_edges, _ = stats.binned_statistic(df.DT, (df.Zrms)**2, bins = bins, statistic='mean')
+    stds, _, _ = stats.binned_statistic(df.DT, (df.Zrms)**2, bins = bins, statistic='std')
+    values_up = meansy + nsigma*stds
+    values_down = meansy - nsigma*stds
+    bin_centers = (bin_edges[:-1]+bin_edges[1:])*0.5
+    f_up = fit(expo, bin_centers, values_up, seed = (meansy.mean(), -1))
+    f_down = fit(expo, bin_centers, values_down, seed = (meansy.mean(), -1))
+
+
 
 def test_f():
 
