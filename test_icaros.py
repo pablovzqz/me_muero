aa1b723457f8d4b3ff79149090dd482b75c212db
Pablo Vazquez <pablovazquez@igfaelaptop.dyndns.cern.ch>
2025-08-05 16:05:32 +0200

    Add test_icaros to commit

---
+++ test_icaros.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pandas as pd
+from scipy.stats import binned_statistic
+
+def diffusion_band(kdst         :   pd.DataFrame,
+                   lower_limit  :   function,
+                   upper_limit  :   function)->pd.DataFrame:
+
+
+    pass
