1cd6a894602c8f0572236c0b24c5031b3af38bea
mcidlaso <mariacidlaso1@gmail.com>
2025-08-06 11:17:58 +0200

    test passed

--- funciones_icaros.py
+++ funciones_icaros.py
@@ -1,30 +1,42 @@
 import pandas as pd
 import numpy as np
-from invisible_cities.core.fit_functions import fit, expo
+from invisible_cities.core.fit_functions import fit, polynom, gauss
+from scipy import stats
 
 def f(df: pd.DataFrame,
       bins: np.ndarray,
       nsigma: float,
       ) -> pd.DataFrame:
-    meansy , bin_edges, _ = stats.binned_statistic(df.DT, (df.Zrms)**2, bins = bins, statistic='mean')
-    stds, _, _ = stats.binned_statistic(df.DT, (df.Zrms)**2, bins = bins, statistic='std')
+
+    Zrms2 = df.Zrms**2
+    meansy , bin_edges, _ = stats.binned_statistic(df.DT, Zrms2, bins = bins, statistic='mean')
+    stds, _, _ = stats.binned_statistic(df.DT, Zrms2, bins = bins, statistic='std')
     values_up = meansy + nsigma*stds
     values_down = meansy - nsigma*stds
     bin_centers = (bin_edges[:-1]+bin_edges[1:])*0.5
-    f_up = fit(expo, bin_centers, values_up, seed = (meansy.mean(), -1))
-    f_down = fit(expo, bin_centers, values_down, seed = (meansy.mean(), -1))
+    f_up = fit(polynom, bin_centers, values_up, seed = (0, 1))
+    f_down = fit(polynom, bin_centers, values_down, seed = (0, 1))
 
+
+    mask = (f_up.fn(df.DT) > Zrms2) & (f_down.fn(df.DT) < Zrms2)
+
+    return df[mask]
 
 
 def test_f():
 
     DT          = np.linspace(0, 1400, 1000)
-    S2e         = 8200 * np.exp(-DT/30000)
+    Zrms         = 8200 * np.exp(-DT/30000) + [1, -1]*(len(DT)//2)
 
-    test_matrix = np.column_stack((DT, S2e))
+    test_matrix = np.column_stack((DT, Zrms))
 
-    df          = pd.DataFrame(test_matrix, columns = ['DT', 'S2e'])
+    df          = pd.DataFrame(test_matrix, columns = ['DT', 'Zrms'])
 
-    assert f(df, 1, 10e10).shape == df.shape
+    var = f(df, 10, 100)
 
-    assert f(df, 1, 0).shape == (0, 2)
+    assert f(df, 10, 100).shape == df.shape
+
+    assert f(df, 10, 0).shape == (0, 2)
+
+
+test_f()
